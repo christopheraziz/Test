a869b767cddcd08f4b06c935aeff1954f87cbf09
Chris <christopheraziz@gmail.com>
2015-12-22 22:59:56 -0500

    slick/3d nav/
    added slick slideshow and 3D rotating navigation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 $( document ).ready(function() {
 
-  $('label').click(function(){
-        $('.image-container img').fadeIn(444);
+  /*
+  *SWITCH
+  */
+  var count = 0;
+  $('#switch').click(function(){
+    var skip = count % 2;
+    if (skip == 0) {
+      $('.image-container img').fadeIn(200);
+      count++;
+    } else {
+      $('.image-container img').fadeOut(200);
+      count++;
+    }
   });
- 
+
+  /*
+  *SLICK SLIDER
+  */
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerMode: true,
+    appendArrows: $('#target')
+  });
+
 });
